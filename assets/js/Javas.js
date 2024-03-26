57775c36cf7cafa48489edb6aa034e40784f09b8
WallalaIronRed <jaranguiz07@gmail.com>
2024-03-26 20:37:54 -0300

    mi primer commit

---
+++ assets/js/Javas.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $(".card-title").click(function(){
+      $(".card-img-top").toggle();
+      });
+      });
+  $(".ing").dblclick(function(){
+      $(this).css({
+      "color": "red",
+      "font-size": "2em"
+      });
+      });
+
+  $(".prep").dblclick(function(){
+      $(this).css({
+      "color": "red",
+      "font-size": "2em"
+      });
+      });
+
+  $(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();   
+  });
+  
+
+
